9cabde6df9fcdde8cc251f9688fcd3c747f9850d
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 21:03:01 +0900

    차이를 최대로

--- 10819.java
+++ 10819.java
@@ -1,9 +1,6 @@
 // 10819 차이를 최대로
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int max = 0;
