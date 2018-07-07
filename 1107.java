e8fab6396d0c2dd375a627040072715c780751a6
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 20:54:32 +0900

    리모컨

--- 1107.java
+++ 1107.java
@@ -1,7 +1,5 @@
 // 1107 리모컨
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
