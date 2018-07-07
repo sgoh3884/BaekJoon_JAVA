58e1fbc9bcd5e3538971b1a816db5a41782cd057
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 20:57:37 +0900

    소수 경로

--- 1963.java
+++ 1963.java
@@ -1,10 +1,6 @@
 // 1963 소수 경로
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
