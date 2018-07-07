721b5bf6991fafa781ad52ac0d44c69f2853e1cc
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 20:47:10 +0900

    별찍기 - 4

--- 2441.java
+++ 2441.java
@@ -1,7 +1,5 @@
 // 2441 별찍기 - 4
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
