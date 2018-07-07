b8c430c1b6f63fa7d53d636454a7ca5b8c216d4f
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 20:50:36 +0900

    기찍 N

--- 2742.java
+++ 2742.java
@@ -1,7 +1,5 @@
 // 2742 기찍 N
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
