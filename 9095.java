47eb249f147efa484d7a86477a6ae5b74f40cd4b
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 21:02:20 +0900

    1, 2, 3 더하기 (함수)

--- 9095.java
+++ 9095.java
@@ -1,8 +1,5 @@
 // 9095 1, 2, 3 더하기
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
