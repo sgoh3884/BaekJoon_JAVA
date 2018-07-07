404ff8aabcf72b081caeaed8384f0692960848d3
sgoh3884 <40913700+sgoh3884@users.noreply.github.com>
2018-07-07 21:42:05 +0900

    A x B

---
+++ 10998.java
@@ -0,0 +1,13 @@
+// 10998 A X B
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		System.out.println(A * B);
+	}
+}
